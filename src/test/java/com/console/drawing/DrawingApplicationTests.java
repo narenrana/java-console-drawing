7f1301d3be89404f9be9cee002c319c67edee680
naren <>
2022-03-09 12:17:32 +0800

    Added init  commit

---
+++ src/test/java/com/console/drawing/DrawingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.console.drawing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrawingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
